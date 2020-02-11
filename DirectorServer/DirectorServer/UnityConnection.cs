8bcf703b87c3e41bf63ea19e53dda21b2a9bdbb2
Ben880 <Ben880@users.noreply.github.com>
2020-02-11 14:47:00 -0500

    Created & Basic connection
    Project created with default web ap setup
    Added UnityConnection
    Added UnityNetConfig
    unity connection will start listening for a connection recieve a packet respond with same packet and then close connection (needed unitil Unity is not sending massive amounts of data)
    UnityNetConfig should be changed to use either JSOn or other

---
+++ DirectorServer/DirectorServer/UnityConnection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DirectorServer
+{
+    public class UnityConnection
+    {
+        private UnityNetConfig netConfig = new UnityNetConfig();
+        TcpListener server;   
+        
+        public UnityConnection()
+        {
+            try
+            {
+                server = new TcpListener(netConfig.Address, netConfig.Port);
+                server.Start();
+                // Buffer for reading data
+                Byte[] bytes = new Byte[1024];
+                String data;
+                // Enter the listening loop.
+                while(true) 
+                {
+                    Console.Write("Waiting for a connection... ");
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();            
+                    Console.WriteLine("Connected!");
+                    data = null;
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    // Loop to receive all the data sent by the client.
+                    while((i = stream.Read(bytes, 0, bytes.Length))!=0) 
+                    {   
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+       
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);            
+                    }
+         
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ DirectorServer/DirectorServer/UnityNetConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace DirectorServer
+{
+    public class UnityNetConfig
+    {
+        private const int portNum = 8052;
+        private const string localAddress = "192.168.0.8";
+        private const bool useLocal = true;
+
+        public int Port
+        {
+            get { return portNum;}
+        }
+        
+        public IPAddress Address
+        {
+            get
+            {
+                if (useLocal)
+                    return Dns.Resolve("localhost").AddressList[0];
+                else
+                    return IPAddress.Parse(localAddress);
+            }
+        }
+    }
+}
