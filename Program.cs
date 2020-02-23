7a833058e24bc155a690359d7efa30f6499181df
Cameron Beyer <cam.beyer@gmail.com>
2020-02-22 19:35:14 -0500

    Re-arranged folder structure, upgraded project to .NET Core 3.1, added .gitignore, fixed networking logic

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace DirectorServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Thread(new ThreadStart(StartUnityServer)).Start();
+            Console.WriteLine("Starting web server");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static void StartUnityServer()
+        {
+            Console.WriteLine("Starting Unity server");
+            _ = new UnityConnection();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
